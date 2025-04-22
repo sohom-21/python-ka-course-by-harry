56129c47eea3506923a823d3dfa6427150da7d0d
Sohom Ghosh <sohom.official21@gmail.com>
2025-04-23 03:35:52 +0530

    Refactor code structure for improved readability and maintainability

---
+++ 42_oops.py
@@ -0,0 +1,11 @@
+class human:
+         def __init__(self, name, age):
+                  self.name = name
+                  self.age = age
+
+#ab banaate hain object:
+person1 = human("sohom",22)
+person2 = human("Riya",21)
+
+print(person1.name)
+print(person2.age)
