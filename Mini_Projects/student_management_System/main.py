4fb72916518dd72fffcee61f980fb9b9179f2267
Sohom Ghosh <139136976+sohom-21@users.noreply.github.com>
2025-06-21 05:50:42 +0530

    feat(student_management): update project configuration and enhance student display functionality

--- Mini_Projects/student_management_System/main.py
+++ Mini_Projects/student_management_System/main.py
@@ -1,10 +1,15 @@
 from student import Student
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, Button, Static
+
 def main():
          s1 = Student("John Doe", "12345", 85)
          s2 = Student("Jane Smith", "67890", 90)
-         s1.display()
-         s2.display()
-
+         # s1.display()
+         # s2.display()
+         s1.tabular_view()
+         s2.tabular_view()
+         
 
 if __name__ == "__main__":
          main()
--- Mini_Projects/student_management_System/student.py
+++ Mini_Projects/student_management_System/student.py
@@ -1,7 +1,8 @@
-from rich import console
+from rich.console import Console
 from rich.table import Table
-from textual import text
+console = Console()
 class Student:
+
          def __init__(self, name, roll, marks):
                   self.name = name
                   self.roll = roll
@@ -11,7 +12,16 @@ class Student:
                   print(f"Student Name : {self.name}")
                   print(f"Roll Number  : {self.roll}")
                   print(f"Marks        : {self.marks}")
-                  
+                  print("\n")
+         
+         def tabular_view(self):
+                 table = Table(title = "student Details")
+                 table.add_column("field", justify = "center", style = "cyan", no_wrap = True)
+                 table.add_column("value", justify = "center", style = "magenta")
+                 table.add_row("Name", self.name)
+                 table.add_row("Roll", str(self.roll))
+                 table.add_row("Marks", str(self.marks))
+                 console.print(table)
 
 class ManageStudents(Student):
          def add_students(self):
